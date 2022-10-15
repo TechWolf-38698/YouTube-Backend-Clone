040ca60404937bb0c9c31c99b410f7c0ccf6cc63
Tech Wolf <techwolf38698@gmail.com>
2022-10-15 07:58:03 +0500

    Update db.js

--- Mongoose/db.js
+++ Mongoose/db.js
@@ -1,9 +1,9 @@
 const mongoose = require("mongoose");
 //Abdur_Rahman
 // arjawad123;
-// const connectionString = "mongodb://localhost:27017/test";
-const connectionString =
-  "mongodb+srv://Abdur_Rehman:arjawad123@yt-practice.oukvq.mongodb.net/?retryWrites=true&w=majority";
+const connectionString = "mongodb://localhost:27017/test";
+// const connectionString =
+//   "mongodb+srv://Abdur_Rehman:arjawad123@yt-practice.oukvq.mongodb.net/?retryWrites=true&w=majority";
 
 const connectMongoose = () => {
   mongoose
